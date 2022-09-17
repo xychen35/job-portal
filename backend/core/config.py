ad234a1486d951773a2a7e1d74b8c112572f3c02
xychen35 <xiaoyangc99@gmail.com>
2022-09-16 23:39:11 -0400

    Updated

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,3 +1,20 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
-    TITLE: str = 'Job Board'
-    VERSION: str = '0.1.0'
+    PROJECT_TITLE: str = 'Job Board'
+    PROJECT_VERSION: str = '0.1.0'
+    
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "db_job_board")
+    
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    
+settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from core.config import Settings
+from db.base_class import Base
+from db.session import engine
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI(title=Settings.TITLE, version=Settings.VERSION)
+app = FastAPI(title=Settings.PROJECT_TITLE, version=Settings.PROJECT_VERSION)
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+Base.metadata.create_all(engine)
 
 @app.get('/')
 async def main():
