6bf5b5422f1f61ba12770b6be53296534c8ef008
xychen35 <xiaoyangc99@gmail.com>
2022-09-16 23:39:18 -0400

    Commit

---
+++ backend/db/base_class.py
@@ -0,0 +1,11 @@
+from typing import *
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+    
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__namme__.lower()
