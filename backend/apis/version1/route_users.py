f905229ec309a31c203f0023136dd71632ea54e4
xychen35 <xiaoyangc99@gmail.com>
2022-12-26 23:00:58 -0500

    Updated Code for Routers

---
+++ backend/apis/version1/route_users.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from schemas.users import UserCreate, ShowUser
+from db.session import get_db
+from db.repository.users import create_new_user
+
+router = APIRouter()
+
+@router.post("/", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Session=Depends(get_db)):
+    user = create_new_user(user, db)
+    return user
--- backend/main.py
+++ backend/main.py
@@ -3,16 +3,19 @@ from core.config import settings
 from db.base import Base
 from db.session import engine
 from fastapi.staticfiles import StaticFiles
+from apis.base import api_router
 
-# app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
-# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
+    
+def include_router(app):
+    app.include_router(api_router)
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
     create_tables()
+    include_router(app)
     return app
 
 app = start_application()
