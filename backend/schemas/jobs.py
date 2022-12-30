c96154405739cf6f18fa81993ad3a4083d30fe78
xychen35 <xiaoyangc99@gmail.com>
2022-12-29 20:32:00 -0500

    Updated

--- backend/schemas/jobs.py
+++ backend/schemas/jobs.py
@@ -8,7 +8,7 @@ class JobBase(BaseModel):
     company_url : Optional[str] = None
     location : Optional[str] = "Remote"
     description: Optional[str] = None
-    date_posted : Optional[str] = datetime.now().date()
+    date_posted : Optional[date] = datetime.now().date()
     
 class JobCreate(JobBase):
     title : str
