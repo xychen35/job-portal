c836b779da4447eb997fcb0890bd38e586f399ee
xychen35 <xiaoyangc99@gmail.com>
2022-12-31 01:48:37 -0500

    Update

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import Session
 
 from schemas.jobs import JobCreate, ShowJob
 from db.session import get_db
-from db.repository.jobs import create_new_job, retreive_job
+from db.repository.jobs import create_new_job, retreive_job, retreive_all_jobs, update_job_by_id
+
+from typing import List
+
 
 router = APIRouter()
 
@@ -14,8 +17,24 @@ def create_job(job: JobCreate, db: Session=Depends(get_db)):
     return job
 
 @router.get("/get/{id}", response_model=ShowJob, status_code=status.HTTP_200_OK)
-def retreive_job_by_id(id: int, db: Session=Depends(get_db)):
+def read_job(id: int, db: Session=Depends(get_db)):
     job = retreive_job(id=id, db=db)
     if not job:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id:{id} does not exist")
     return job
+
+@router.get("/all-jobs", response_model=List[ShowJob], status_code=status.HTTP_200_OK)
+def list_jobs(db: Session=Depends(get_db)):
+    jobs = retreive_all_jobs(db=db)
+    if not jobs:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is not avaiable job in the Jobs database")
+    return jobs
+
+@router.put("/update-job/{id}", status_code=status.HTTP_200_OK)
+def update_job(id: int, job: JobCreate, db: Session=Depends(get_db)):
+    owner_id = 1
+    message = update_job_by_id(id=id, job=job, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id:{id} does not exist")
+    return {"detail": "Successfully updated"}
+    
--- backend/db/repository/jobs.py
+++ backend/db/repository/jobs.py
@@ -14,6 +14,17 @@ def create_new_job(job: JobCreate, db: Session, owner_id: int):
 
 def retreive_job(id: int, db: Session):
     job = db.query(Job).filter(Job.id==id).first()
-    if not job:
-        HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id:{id} does not exist")
     return job
+
+def retreive_all_jobs(db:Session):
+    jobs = db.query(Job).filter(Job.is_active==True).all()
+    return jobs
+
+def update_job_by_id(id: int,job: JobCreate, db: Session, owner_id):
+    existing_job = db.query(Job).filter(Job.id == id)
+    if not existing_job.first():
+        return 0
+    job.__dict__.update(owner_id=owner_id)
+    existing_job.update(job.__dict__)
+    db.commit()
+    return 1
