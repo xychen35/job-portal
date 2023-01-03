a4c4d246f478026ae37be857bd49abe4e7cf7c50
xychen35 <xiaoyangc99@gmail.com>
2023-01-03 18:32:34 -0500

    Updated code for tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 import sys
 import os
@@ -13,6 +13,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from db.base import Base
 from db.session import get_db
 from apis.base import api_router
+from tests.utils.users import authentication_token_from_email
+from core.config import settings
 
 def start_application():
     app = FastAPI()
@@ -26,7 +28,7 @@ engine = create_engine(
 
 SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def app() -> Generator[FastAPI, Any, None]:
     """
     Create a fresh database on each test case.
@@ -36,7 +38,7 @@ def app() -> Generator[FastAPI, Any, None]:
     yield _app
     Base.metadata.drop_all(engine)
     
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
     connection = engine.connect()
     transaction = connection.begin()
@@ -47,7 +49,7 @@ def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
     connection.close()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def client(
     app: FastAPI, db_session: SessionTesting
 ) -> Generator[TestClient, Any, None]:
@@ -65,3 +67,7 @@ def client(
     app.dependency_overrides[get_db] = _get_test_db
     with TestClient(app) as client:
         yield client
+        
+@pytest.fixture(scope="module")
+def normal_user_token_headers(client: TestClient, db_session: Session):
+    return  authentication_token_from_email(client=client, email=settings.TEST_USER_EMAIL, db=db_session)
--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -1,29 +1,30 @@
 import json
 
 
-def test_create_job(client):
+def test_create_job(client, normal_user_token_headers):
     data = {
-        "title":"SDE", 
-        "company":"GOOGLE", 
-        "company_url":"GOOGLE",
-        "location":"LAS",
-        "description":"GOOD",
-        "date_posted":"2022-12-28"
-        }
-    response = client.post("/jobs/create-job", json.dumps(data))
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20",
+    }
+    response = client.post("/jobs/create-job", data=json.dumps(data), headers=normal_user_token_headers)
     assert response.status_code == 201
-    assert response.json()["company"] == "GOOGLE"
+    assert response.json()["company"] == "doogle"
+    assert response.json()["description"] == "python"
     
-def test_retreive_job_by_id(client):
+def test_retreive_job_by_id(client, normal_user_token_headers):
     data = {
-        "title":"SDE", 
-        "company":"GOOGLE", 
-        "company_url":"GOOGLE",
-        "location":"LAS",
-        "description":"GOOD",
-        "date_posted":"2022-12-28"
-        }
-    client.post("/jobs/create-job", json.dumps(data))
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20",
+    }
+    client.post("/jobs/create-job", json.dumps(data), headers=normal_user_token_headers)
     response = client.get("/jobs/get/1")
     assert response.status_code == 200
-    assert response.json()["title"] == "SDE"
+    assert response.json()["title"] == "SDE super"
